6e9ef0d781686d56dfbc095994397184b70c29e1
Ryo Suzuki <astronautryosuzuki@gmail.com>
2023-01-12 23:43:38 +0900

    add:

--- curl-MSM-s.sh
+++ curl-MSM-s.sh
@@ -8,6 +8,6 @@ cd $year
 mkdir $month
 cd $month
 
-for j in {01..31}; do
+for j in `seq -w 1 31`; do
     curl -O "http://database.rish.kyoto-u.ac.jp/arch/jmadata/data/gpv/netcdf/MSM-S/$year/$month$j.nc"
 done
