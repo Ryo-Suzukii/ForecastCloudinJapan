b1da9cc65106e3b1adfcf6f61f00f5d9b3f6e260
Ryo Suzuki <astronautryosuzuki@gmail.com>
2023-02-11 19:13:45 +0900

    change:年を指定し指定した期間すべてのMSMデータをダウンロードする

--- curl-MSM-s.sh
+++ curl-MSM-s.sh
@@ -1,13 +1,26 @@
-year=$1
-month=$2
-
 mkdir data
 cd data
-mkdir $year
-cd $year
-mkdir $month
-cd $month
 
-for j in `seq -w 1 31`; do
-    curl "http://database.rish.kyoto-u.ac.jp/arch/jmadata/data/gpv/netcdf/MSM-S/$year/$month$j.nc" -o ${year}_${month}${j}.nc
+for i in `seq 2022 2022`; do
+  mkdir $i
+  cd $i
+  for j in `seq -w 1 12`; do
+    mkdir $j
+    cd $j
+    for k in `seq -w 1 31`; do
+      year=$i
+      month=$j
+      day=$k
+
+      if [ ! -f "${year}_${month}${day}.nc" ]; then
+        curl "http://database.rish.kyoto-u.ac.jp/arch/jmadata/data/gpv/netcdf/MSM-S/$year/$month$day.nc" -o ${year}_${month}${day}.nc
+        echo "download ${year}_${month}${day}.nc"
+      else
+        echo "skip ${year}_${month}${day}.nc"
+      fi
+    done
+    cd ../
+  done
+  cd ../
 done
+cd ../
